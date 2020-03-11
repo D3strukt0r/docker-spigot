f5a5ae6029af7aee808a03fc5ee9724d59d521d8
Manuele Vaccari <manuele.vaccari@gmail.com>
2020-03-11 23:14:00 +0100

    Fix wrong argument

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,4 @@ if [ "$EULA" == "true" ] && [ ! -s eula.txt ]; then
     echo -e "#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).\n#$(date)\neula=true" > eula.txt
 fi
 
-java -Xms"${JAVA_BASE_MEMORY}" -Xmx"${JAVA_MAX_MEMORY}" -jar /app/spigot.jar --no-gui
+java -Xms"${JAVA_BASE_MEMORY}" -Xmx"${JAVA_MAX_MEMORY}" -jar /app/spigot.jar --nogui
