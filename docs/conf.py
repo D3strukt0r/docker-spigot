a4e9a22c9b1e002e7c102d70f1359e75aa64bcc1
Manuele Vaccari <manuele.vaccari@gmail.com>
2020-03-13 16:42:14 +0100

    Use default theme

--- docs/conf.py
+++ docs/conf.py
@@ -14,9 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-# Make sure to run "pip install -r requirements.txt" first!
-import sphinx_rtd_theme
-
 # -- Project information -----------------------------------------------------
 
 project = 'docker-spigot'
@@ -30,7 +27,6 @@ author = 'Manuele Vaccari'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,7 +43,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
