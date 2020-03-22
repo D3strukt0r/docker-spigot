b0fc0b740463d57eb03f83f0be3dfd10c3aa62d3
Manuele Vaccari <manuele.vaccari@gmail.com>
2020-03-22 12:38:39 +0100

    Rename variable for naming convention

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -52,10 +52,10 @@ JAVA_OPTIONS="-Xms${JAVA_BASE_MEMORY} -Xmx${JAVA_MAX_MEMORY} ${JAVA_OPTIONS}"
 echo -e "\e[1A[ \e[32mOK\e[39m ]"
 
 # Console buffers
-console_input="/app/input.buffer"
+_console_input="/app/input.buffer"
 # Clear console buffers
-> $console_input
+true > $_console_input
 
 # Start the main application
 echo "[    ] Starting Minecraft server..."
-tail -f $console_input | tee /dev/console | java $JAVA_OPTIONS -jar /app/spigot.jar --nogui "$@"
+tail -f $_console_input | tee /dev/console | java $JAVA_OPTIONS -jar /app/spigot.jar --nogui "$@"
