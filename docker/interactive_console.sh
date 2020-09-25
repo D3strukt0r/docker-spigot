4129270cb030f1b705e9d667c6760cae8cf9ca07
Manuele Vaccari <manuele.vaccari@gmail.com>
2020-09-25 16:32:38 +0200

    Fix buffer has to be in /tmp

--- docker/interactive_console.sh
+++ docker/interactive_console.sh
@@ -3,4 +3,4 @@
 # @author https://stackoverflow.com/questions/62049537/send-commands-directly-in-running-process-and-indirectly-e-g-with-tail
 while IFS= read -rp '$ ' command; do
     printf '%s\n' "$command"
-done >>/app/input.buffer
+done >>/tmp/input.buffer
